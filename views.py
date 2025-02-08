30266c0d80222576b8db9bc1f1f7cda5d1f5be60
lin9178618872 <62189686+lin9178618872@users.noreply.github.com>
2025-02-07 21:51:40 -0500

    Delete views.py

--- views.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import viewsets
-from .models import Medication
-from .serializers import MedicationSerializer
-
-class MedicationViewSet(viewsets.ModelViewSet):
-    queryset = Medication.objects.all()
-    serializer_class = MedicationSerializer
